a4128a2906238c0e71f9edc34b91b55768a79b87
JAMES KABOGO NJOROGE <kabogp@gmail.com>
2024-01-31 23:04:55 +0300

    added a nav bar toogle button using css and javascript

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const navToggle = document.getElementById("navToggle");
+    const mobileLinks = document.querySelector(".mobile-links");
+
+    navToggle.addEventListener("click", function () {
+        mobileLinks.classList.toggle("hidden");
+        
+    });
+    // setTimeout(function () {
+    //     mobileLinks.classList.toggle("hidden");
+    // }, 3000);
+    
+    mobileLinks.addEventListener("click", function () {
+        mobileLinks.classList.toggle("hidden");
+    });
+});
 
