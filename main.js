3b69c09cb6de8b4d1fc24b36273c19abc13ab72a
JAMES KABOGO NJOROGE <kabogp@gmail.com>
2024-02-21 09:38:13 +0300

    added a code to ensure that the page clicked will show on the nav bar using purple colour

--- main.js
+++ main.js
@@ -15,3 +15,16 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    navLinks.forEach(function(navLink) {
+        navLink.addEventListener('click', function() {
+            navLinks.forEach(function(link) {
+                link.classList.remove('active');
+            });
+            this.classList.add('active');
+        });
+    });
+});
+
